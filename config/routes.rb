49aef4681ccd8a34ceb369cd4206f00e175962ca
EdMir <canonnist@gmail.com>
2022-09-07 23:05:14 +0200

    change home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
  end
- 
+
   resources :friends
   #get 'home/index'
-  root 'home#index'
+  root 'friends#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'home/about'
   # Defines the root path route ("/")
